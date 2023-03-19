532670fff1654a8018a76787c9c8a18e19c20994
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-03-19 15:27:30 +0200

    Author entity DAL

---
+++ Server/Repositories/AuthorRepository.cs
@@ -0,0 +1,19 @@
+﻿using Server.Models;
+using Server.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Repositories
+{
+    internal class AuthorRepository : GenericRepository<Author>, IAuthorReposiitory
+    {
+        public AuthorRepository(SqlConnection sqlConnection, IDbTransaction dbTransaction, string tableName) : base(sqlConnection, dbTransaction, tableName)
+        {
+        }
+    }
+}
