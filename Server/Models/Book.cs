c248cfe66f1bc8255005469b3eac5c3adb1901e2
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-04-22 21:53:37 +0300

    Added new models

---
+++ Server/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Server.Models
+{
+    internal class Book : BaseModel
+    {
+        public string? Isbn { get; set; }
+        public int NumberOfPages { get; set; }
+        public int PublishingYear { get; set; }
+        public long AuthorId { get; set; }
+        public long PublisherId { get; set; }
+        public long GenreId { get; set; }
+
+        [DescriptionAttribute("ignore")]
+        public string? AuthorName { get; set; }
+        [DescriptionAttribute("ignore")]
+        public string? PublisherName { get; set; }
+        [DescriptionAttribute("ignore")]
+        public string? GenreName { get; set; }
+
+    }
+}
---
+++ Server/Models/Exemplar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    internal class Exemplar
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsLend { get; set; }
+        public long BookId { get; set; }
+
+        [DescriptionAttribute("ignore")]
+        public string? BookName { get; set; }
+    }
+}
